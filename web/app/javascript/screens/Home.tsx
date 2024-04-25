9eb58c6f0c1622f9377e50297f0ae0a0f536092f
yamayu1 <yuuyayamada1@gmail.com>
2024-04-26 00:56:40 +0900

    gem追加

--- web/app/javascript/screens/Home.tsx
+++ web/app/javascript/screens/Home.tsx
@@ -5,9 +5,20 @@ function Home() {
   return (
     <>
       <h1>Hello World</h1>
-      <Link to="/test">テストページへ</Link>
+      <Link to="/test" style={buttonStyle}>新規登録</Link>
     </>
   );
 }
+
+const buttonStyle = {
+  display: 'inline-block',
+  padding: '5px 10px',
+  fontSize: '16px',
+  backgroundColor: '#007bff',
+  color: '#fff',
+  borderRadius: '5px',
+  textDecoration: 'none',
+  cursor: 'pointer',
+};
 
 export default Home;
